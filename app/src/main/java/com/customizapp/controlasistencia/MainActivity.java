ad26f9e72cb8cd547f12c2aeae243de80c57302d
Victor <victor.martinez@customizapp.com>
2019-03-01 10:26:06 -0600

    El Paso de la Muerte

--- app/src/main/java/com/customizapp/controlasistencia/MainActivity.java
+++ app/src/main/java/com/customizapp/controlasistencia/MainActivity.java
@@ -1,10 +1,15 @@
 package com.customizapp.controlasistencia;
 
+
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +44,20 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        Button btn = (Button)findViewById(R.id.btnPicale);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Inicio.class));
+            }
+        });
+
+
+
+
     }
 
 }
