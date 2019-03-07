6e615c0dcb9f1c71ba7cbfc4a2c7763e653d5f37
juarezj <juarezj@customizapp.com>
2019-03-06 20:24:40 -0600

    Creacion de la clase usuario, se añadio dentro del Main Activity, jala sin problemas hasta ahora...

---
+++ app/src/main/java/com/customizapp/controlasistencia/Usuario.java
@@ -0,0 +1,169 @@
+package com.customizapp.controlasistencia;
+
+public class Usuario {
+
+    private String id;
+
+    private String idExterno;
+
+    private String nombre;
+
+    private String apPaterno;
+
+    private String apMaterno;
+
+    private String vigenciaInicio;
+
+    private String VigenciaFin;
+
+    private String idEstatus;
+
+    private String fechaAlta;
+
+    private String fechaModificacion;
+
+    private Boolean activo;
+
+    private Byte fotoPerfil;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String nombreUsuario;
+
+    private String contrasena;
+
+    public Usuario(){
+
+        setNombreUsuario("Usuario");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdExterno() {
+        return idExterno;
+    }
+
+    public void setIdExterno(String idExterno) {
+        this.idExterno = idExterno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApPaterno() {
+        return apPaterno;
+    }
+
+    public void setApPaterno(String apPaterno) {
+        this.apPaterno = apPaterno;
+    }
+
+    public String getApMaterno() {
+        return apMaterno;
+    }
+
+    public void setApMaterno(String apMaterno) {
+        this.apMaterno = apMaterno;
+    }
+
+    public String getVigenciaInicio() {
+        return vigenciaInicio;
+    }
+
+    public void setVigenciaInicio(String vigenciaInicio) {
+        this.vigenciaInicio = vigenciaInicio;
+    }
+
+    public String getVigenciaFin() {
+        return VigenciaFin;
+    }
+
+    public void setVigenciaFin(String vigenciaFin) {
+        VigenciaFin = vigenciaFin;
+    }
+
+    public String getIdEstatus() {
+        return idEstatus;
+    }
+
+    public void setIdEstatus(String idEstatus) {
+        this.idEstatus = idEstatus;
+    }
+
+    public String getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(String fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public String getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(String fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public Byte getFotoPerfil() {
+        return fotoPerfil;
+    }
+
+    public void setFotoPerfil(Byte fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
